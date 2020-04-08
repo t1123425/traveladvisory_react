4db0f6ce304290c1c110f1fe47983fdacffb81d9
tom yuan <t1123425@gmail.com>
2020-04-08 18:06:24 +0800

    upload to gh-page

---
+++ precache-manifest.a8374a67226829af3ce7530d3af6430b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb26cc456d2caa4c5ea7efe206fe3e12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d3c52947a08b582f607",
+    "url": "/static/css/main.18b94ceb.chunk.css"
+  },
+  {
+    "revision": "b2dda528a53066aabb3c",
+    "url": "/static/js/2.afd8da6a.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/static/js/2.afd8da6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d3c52947a08b582f607",
+    "url": "/static/js/main.6f263f3f.chunk.js"
+  },
+  {
+    "revision": "b4f442a4e7ed59e8ad76",
+    "url": "/static/js/runtime-main.22aa8fad.js"
+  }
+]);
