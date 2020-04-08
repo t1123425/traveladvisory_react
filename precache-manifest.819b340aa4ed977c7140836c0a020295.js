04dbf763b59ab562bb63dd0c349bef321b7f64fe
tom yuan <t1123425@gmail.com>
2020-04-08 18:20:56 +0800

    fix path

---
+++ precache-manifest.819b340aa4ed977c7140836c0a020295.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84259e4ef775176669a17ba999f5d06c",
+    "url": "/traveladvisory_react/index.html"
+  },
+  {
+    "revision": "88da0387493c4ec29a12",
+    "url": "/traveladvisory_react/static/css/main.18b94ceb.chunk.css"
+  },
+  {
+    "revision": "b2dda528a53066aabb3c",
+    "url": "/traveladvisory_react/static/js/2.afd8da6a.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/traveladvisory_react/static/js/2.afd8da6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88da0387493c4ec29a12",
+    "url": "/traveladvisory_react/static/js/main.4cfd8be6.chunk.js"
+  },
+  {
+    "revision": "3e09049864a0016a54f8",
+    "url": "/traveladvisory_react/static/js/runtime-main.0244d17f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a8374a67226829af3ce7530d3af6430b.js"
+  "/traveladvisory_react/precache-manifest.819b340aa4ed977c7140836c0a020295.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/traveladvisory_react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
